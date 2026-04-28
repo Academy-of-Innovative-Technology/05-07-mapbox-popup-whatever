f1fe0aab83b92cad576ea3141522f253819b20a4
u-a-b <144457851+u-a-b@users.noreply.github.com>
2026-04-28 13:44:27 -0400

    complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = ' ACCESS TOKEN ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidWEyIiwiYSI6ImNtbnl3djZ2azA2bHMycm9nb3MycmZpN3UifQ._jWrIGrPo-6izrQe9cfgrw';
 
 var mapProps = {
 	container: 'map',
@@ -8,3 +8,7 @@ var mapProps = {
 
 var map = new mapboxgl.Map(mapProps);
 
+let popup = new mapboxgl.Popup({ closeOnClick: false})
+.setLngLat([-73.97, 40.75])
+.setHTML('<h1>I <span><3</span> NY</h1>')
+.addTo(map);
